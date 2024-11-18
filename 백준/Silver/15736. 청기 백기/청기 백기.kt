0972cbb5a716d321dbafa6f744c150bb39d2d69c
spicypunch <72846127+spicypunch@users.noreply.github.com>
2024-11-18 18:07:24 +0900

    [Silver IV] Title: 청기 백기, Time: 76 ms, Memory: 12172 KB -BaekjoonHub

---
+++ 백준/Silver/15736. 청기 백기/청기 백기.kt
@@ -0,0 +1,12 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import kotlin.math.sqrt
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+
+    val answer = sqrt(n.toDouble()).toInt()
+
+    println(answer)
+}
